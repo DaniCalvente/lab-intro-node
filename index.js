df73807ab6a405893c3901993aee5958584b1c04
Dani Calvente <danicalvente@hotmail.com>
2022-02-15 12:57:53 +0100

    todas las iteraciones y empezando bonus

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    let sortedItems = this.items.sort((firstItem, secondItem) => {
+      if ( firstItem > secondItem){
+        return 1
+      } else {
+        return -1
+      }
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if ( this.items.indexOf(pos) === -1) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos]
+    }
+    
+  } 
 
-  max() {}
+  max() {
+    if ( this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if ( this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+      this.items.reduce(( acc, element) => {
+        return acc + element
+      } ,0);
+    
+      if (this.items.length === 0) {
+        return 0
+      }
+    
+  }
+  // ESTAMOS TRATANDO DE UTILIZAR METODO REDUCE PARA RETORNAR UN UNICO VALOR, CON EL ACUMULADO DE TODOS, PERO NO CONSEGUIMOS IMPLEMENTARLO
 
   avg() {}
 }
